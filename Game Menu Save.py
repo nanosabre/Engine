71499b31d4ad23d4f9f9f0827147ebf29bbcc97a
Nanosabre <nanosabre@gmail.com>
2023-09-06 22:30:50 -0400

    Pre-commented

--- Game Menu Save.py
+++ Game Menu Save.py
@@ -4,17 +4,17 @@ import csv
 resolution = [768, 768]
 pygame.init()
 
+#cd D:\Files\Code\Python\Engine
 
 font = pygame.font.SysFont(None, 24)
 
-def renderText(text, size, pos:int = 0, typef = font, color = (0,0,0), background = None):
+def renderText(text:str, size:tuple, pos:int = 0, b:pygame.Surface = None, border:int = 5, typef = font, color = (0,0,0)):
     print("TEXT RENDER CALL")
-    width = size[0]
-    height = size[1]
+    background = b
+    width = size[0] - 2*border
+    height = size[1] - 2*border
     holdPos = 0
     start = 0
-    #surf = pygame.Surface((width, height), pygame.SRCALPHA, (255, 255, 255))
-    surf = pygame.transform.scale(pygame.image.load("Assets" + "\\" + str("Menu Back.png")), size)
     subsurfs = []
     j = 0
     for i in range(len(text)):
@@ -44,33 +44,38 @@ def renderText(text, size, pos:int = 0, typef = font, color = (0,0,0), backgroun
     elif pos < 0:
         pos = 0
     for i in range(len(subsurfs)):
-        surf.blit(subsurfs[i], (0, 24*0.9*i - pos))
-    return surf, pos
+        background.blit(subsurfs[i], (border, 24*0.9*i - pos + border))
+    return background, pos
 
 
 class UIElement:
-    def __init__(self, name, layer, pos, size, att = None, file = None):
+    def __init__(self, name, layer, pos, size, att = None, data = None):
         self.name = name
         self.layer = layer
         self.rect = pygame.Rect(pos, size)
         self.att = att
 
         if "image" in att:
-            self.loadImg(file)
+            self.surf = self.loadImg(data[0])
+        else:
+            self.surf = pygame.Surface(size, pygame.SRCALPHA, (0, 0, 0, 0))
         if "text" in att:
-            with open("Assets" + "\\" + file, 'r') as textFile:
+            with open("Assets" + "\\" + data[1], 'r') as textFile:
                 self.text = textFile.read()
             self.scrollpos = 0
+            self.background = pygame.Surface(self.rect.size, pygame.SRCALPHA, (0, 0, 0, 0)) #CREATES BACKGROUND SURFFACE AND LOADS IMAGE
             self.loadText()
-
+        
     def resize(self, size):
         self.rect = pygame.Rect(self.rect.x, self.rect.y, size[0], size[1])
     
     def loadImg(self, file):
-        self.surf = pygame.transform.scale(pygame.image.load("Assets" + "\\" + str(file)), (self.rect.width, self.rect.height))
+        return pygame.transform.scale(pygame.image.load("Assets" + "\\" + str(file)), (self.rect.width, self.rect.height))
 
     def loadText(self, dpos = 0):
-        self.surf, self.scrollpos = renderText(self.text, self.rect.size, self.scrollpos + dpos)
+        self.background = pygame.Surface(self.rect.size, pygame.SRCALPHA, (0, 0, 0, 0))
+        self.surf = pygame.Surface(self.rect.size, pygame.SRCALPHA, (0, 0, 0, 0))
+        self.surf, self.scrollpos = renderText(self.text, self.rect.size, self.scrollpos + dpos, self.background)
 
 
 UIList = []
@@ -79,7 +84,8 @@ focused = []
 with open("Main Menu.csv", newline='') as menuCSV:
     reader = csv.DictReader(menuCSV)
     for row in reader:
-        UIList.append(UIElement(row['name'], int(row['layer']),(int(row['x']), int(row['y'])), (int(row['width']), int(row['height'])), row['attributes'], row['file']))
+        data = [row['data1'], row['data2'], row['data3']]
+        UIList.append(UIElement(row['name'], int(row['layer']),(int(row['x']), int(row['y'])), (int(row['width']), int(row['height'])), row['attributes'], data))
 
 
 screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
@@ -132,7 +138,7 @@ while running:
     if len(focused) > 0:
         screen.blit(focused[0].surf, focused[0].rect)
 
-    
+
     #RENDER FRAME
     pygame.display.flip()
 
